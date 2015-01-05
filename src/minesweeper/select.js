bf18c894a742b621c19877e88f2cf7aa60690829
Will Shown <w@willshown.com>
2015-01-04 19:22:53 -0800

    Selecting perimeter and diagonally adjacent zero-adjacency fields.

--- src/minesweeper/select.js
+++ src/minesweeper/select.js
@@ -2,24 +2,53 @@ define(['underscore'], function(_){
 
   return function(xstr, ystr){
 
-    var x = parseInt(xstr, 10),
+    var self = this,
+        x = parseInt(xstr, 10),
         y = parseInt(ystr, 10),
-        selected = [[x,y]];
+        selected = [[x,y]],
+        perimeter = [],
+        cx, cy;
 
     if(this.board[x][y].adjacent === 0){
 
       var stack = [selected[0]];
 
+      var checkDiagonal = function(dx, dy){
+        if (!self.board[dx][dy].revealed) {
+          if (self.board[dx][dy].adjacent === 0) {
+            stack.push([dx,dy]);
+          } else {
+            perimeter.push([dx,dy]);
+            self.board[dx][dy].revealed = true;
+          }
+        }
+      };
+
       while(stack.length > 0){
         var cur = stack.pop(),
-            cx = cur[0],
-            cy = cur[1],
             left = false,
             right = false;
+
+        cx = cur[0];
+        cy = cur[1];
 
         // move upward
         while(cy - 1 >= 0 && this.board[cx][cy - 1].adjacent === 0){
           cy -= 1;
+        }
+
+        // add upper cap to perimeter
+        if(cy - 1 >= 0){
+          perimeter.push([cx, cy - 1]);
+          this.board[cx][cy-1].revealed = true;
+
+          if(cx-1 >= 0) {
+            checkDiagonal(cx - 1, cy - 1);
+          }
+
+          if (cx + 1 < this.width) {
+            checkDiagonal(cx + 1, cy - 1);
+          }
         }
 
         // fill downward
@@ -33,10 +62,15 @@ define(['underscore'], function(_){
 
           // check for stack opportunities
           if(cx > 0){
-            if(this.board[cx-1][cy].adjacent === 0 && !this.board[cx-1][cy].revealed){
-              if(!left){
-                stack.push([cx-1,cy]);
-                left = true;
+            if(!this.board[cx-1][cy].revealed){
+              if(this.board[cx-1][cy].adjacent === 0) {
+                if (!left) {
+                  stack.push([cx - 1, cy]);
+                  left = true;
+                }
+              }else{
+                perimeter.push([cx - 1, cy]);
+                this.board[cx - 1][cy].revealed = true;
               }
             }else
             if(left){
@@ -45,10 +79,15 @@ define(['underscore'], function(_){
           }
 
           if(cx < this.width - 1){
-            if(this.board[cx+1][cy].adjacent === 0 && !this.board[cx+1][cy].revealed){
-              if(!right){
-                stack.push([cx+1,cy]);
-                right = true;
+            if(!this.board[cx+1][cy].revealed){
+              if(this.board[cx+1][cy].adjacent === 0) {
+                if (!right) {
+                  stack.push([cx + 1, cy]);
+                  right = true;
+                }
+              }else{
+                perimeter.push([cx + 1, cy]);
+                this.board[cx + 1][cy].revealed = true;
               }
             }else
             if(right){
@@ -60,11 +99,25 @@ define(['underscore'], function(_){
 
         } // done filling downward
 
+        // add lower cap to perimeter
+        if(cy < this.height){
+          perimeter.push([cx, cy]);
+          this.board[cx][cy].revealed = true;
+
+          if(cx-1 >= 0){
+            checkDiagonal(cx - 1, cy);
+          }
+
+          if(cx+1 < this.width){
+            checkDiagonal(cx+1, cy);
+          }
+        }
+
       } // done with stack
 
     } // done selecting all empty tiles
 
-    return selected;
+    return selected.concat(perimeter);
 
   };
 
